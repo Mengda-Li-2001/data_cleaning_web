e4ae6b622061eeac2d0792be1fea0eaeb3d0364e
RL <2049780670@qq.com>
2023-11-20 19:43:03 +0800

    Revert "Revert "监听的主机改为0.0.0.0""
    This reverts commit 2dd0394e549483e78a357f7a3d0e4ebaf094bce4.

--- web程序/settings.py
+++ web程序/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_mn#6m!apf3hqb!$sh@s7!$-)rcriuc3f0nkwtyxv1n8#nv48p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
